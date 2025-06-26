272e9363806b009b727ec78a6e6c9e6030114858
Anubhav Srivastava <134841754+ig-void@users.noreply.github.com>
2025-06-26 14:04:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(cors({
   origin: "http://localhost:3000", // your Vercel URL http://localhost:3000
-  credentials: true
+  
 }))
 app.use(express.json())
 app.use("/uploads", express.static("uploads"))
